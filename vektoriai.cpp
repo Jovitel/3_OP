3bc4783c0a53f88ca54c00f46e872395dd338e94
Jovitel <jovita.venck@gmail.com>
2024-02-20 15:52:04 +0200

    new functions func_input file

--- vektoriai.cpp
+++ vektoriai.cpp
@@ -4,6 +4,10 @@
 #include <numeric>
 #include <iomanip>
 #include <ctime>
+#include <fstream>
+#include <istream>
+#include <sstream>
+#include <numeric>
 
 using namespace std;
 
@@ -14,7 +18,7 @@ struct duomenys {
     vector<int> nd;
     int egzaminas;
     double gal_vid, gal_bal, gal_med;
-    int nd_kiekis;
+     int nd_kiekis;
 };
 
 void func_input_hands() {
@@ -329,6 +333,103 @@ void func_generate_names() {
     }
 }
 
+void func_input_file()
+{
+vector<duomenys> studentai;
+    ifstream fd("kursiokai.txt");
+    string line;
+    
+    while (getline(fd, line)) {
+        istringstream iss(line);
+        duomenys studentas;
+        iss >> studentas.vard >> studentas.pav;
+        int pazymys;
+        while (iss >> pazymys) {
+            studentas.nd.push_back(pazymys);
+        }
+        iss.clear();
+        iss >> studentas.egzaminas;
+        studentai.push_back(studentas);
+    }
+
+    fd.close();
+
+    // Apskaičiuojame vidurkį ir medianą
+    for (int i = 0; i < studentai.size(); ++i) {
+        vector<int> pazymiai = studentai[i].nd;
+        sort(pazymiai.begin(), pazymiai.end());
+
+        int dydis = pazymiai.size();
+        if (!pazymiai.empty()) {
+    sort(pazymiai.begin(), pazymiai.end());
+
+    int dydis = pazymiai.size();
+    if (dydis == 1) {
+        studentai[i].gal_med = pazymiai[0];
+    } else if (dydis % 2 == 0) {
+        studentai[i].gal_med = (pazymiai[dydis / 2 - 1] + pazymiai[dydis / 2]) / 2.0;
+    } else {
+        studentai[i].gal_med = pazymiai[dydis / 2];
+    }
+} else {
+    // Handle case when no grades are available
+    studentai[i].gal_med = 0.0; // or any other appropriate value
+}
+    }
+    
+    // Išvestis į failą arba į ekraną
+    ofstream fdd("rezultatai.txt");
+    int s, rez;
+
+    cout << "Jei norite išvesti MEDIANĄ, įrašykite 1, o jei norite išvesti GALUTINĮ BALĄ, įrašykite 0" << endl;
+    while (true) {
+        cin >> s;
+        if (cin.fail() || (s != 0 && s != 1)) {
+            cout << "Įrašėte netinkamą skaičių, rinkitės iš 1 ir 0: ";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        } else {
+            break;
+        }
+    }
+
+    cout << "Jei rezultatus norite išvesti ekrane spauskite 1," << endl;
+    cout << " o jei norite išvesti į failą (rezultatai.txt), spauskite 0" << endl;
+    cin >> rez;
+
+    if (rez == 1) {
+        if (s == 1) {
+            cout << left << setw(20) << "VARDAS" << setw(20) << "PAVARDĖ" << setw(10) << "GALUTUNIS (MED.)" << endl;
+            cout << "------------------------------------------------------------------------" << endl;
+            for (int i = 0; i < studentai.size(); i++) {
+                cout << left << setw(20) << studentai[i].vard << setw(20) << studentai[i].pav << setw(10) << fixed << setprecision(2) << studentai[i].gal_med << endl;
+            }
+        } else if (s == 0) {
+            cout << left << setw(20) << "VARDAS" << setw(20) << "PAVARDĖ" << setw(10) << "GALUTUNIS (VID.)" << endl;
+            cout << "------------------------------------------------------------------------" << endl;
+            for (int i = 0; i < studentai.size(); i++) {
+                cout << left << setw(20) << studentai[i].vard << setw(20) << studentai[i].pav << setw(10) << fixed << setprecision(2) << studentai[i].gal_vid << endl;
+            }
+        }
+    } else if (rez == 0) {
+        if (s == 1) {
+            fdd << left << setw(20) << "VARDAS" << setw(20) << "PAVARDĖ" << setw(10) << "GALUTUNIS (MED.)" << endl;
+            fdd << "------------------------------------------------------------------------" << endl;
+            for (int i = 0; i < studentai.size(); i++) {
+                fdd << left << setw(20) << studentai[i].vard << setw(20) << studentai[i].pav << setw(10) << fixed << setprecision(2) << studentai[i].gal_med << endl;
+            }
+        } else if (s == 0) {
+            fdd << left << setw(20) << "VARDAS" << setw(20) << "PAVARDĖ" << setw(10) << "GALUTUNIS (VID.)" << endl;
+            fdd << "------------------------------------------------------------------------" << endl;
+            for (int i = 0; i < studentai.size(); i++) {
+                fdd << left << setw(20) << studentai[i].vard << setw(20) << studentai[i].pav << setw(10) << fixed << setprecision(2) << studentai[i].gal_vid << endl;
+            }
+        }
+    }
+
+    fdd.close();
+}
+
 int main() {
      int choice;
 
@@ -337,10 +438,11 @@ int main() {
     cout << "2 - sugeneruoti pažymius" << endl;
     cout << "3 - sugeneruoti pažymius ir vardus" << endl;
     cout << "4 - užbaigti programą" << endl;
+    cout << "5 - duomenis nuskaityti iš failo" << endl;
     cin >> choice;
 
-    while (cin.fail() || (choice < 1 || choice > 4)) {
-        cout << "Netinkamas pasirinkimas. Įveskite 1, 2, 3 arba 4: ";
+    while (cin.fail() || (choice < 1 || choice > 5)) {
+        cout << "Netinkamas pasirinkimas. Įveskite 1, 2, 3, 4 arba 5: ";
         cin.clear();
         cin.ignore(10000, '\n');
         cin >> choice;
@@ -354,6 +456,8 @@ int main() {
         func_generate_names();
     } else if (choice == 4) {
         cout<<"Užbaigėte";
-    } 
+    } else if (choice == 5) {
+        func_input_file();
+    }
     return 0;
 }
