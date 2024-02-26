6d3fcfe5c77d3b056c9b2354ac8b863d3ccd5c4a
Jovitel <jovita.venck@gmail.com>
2024-02-26 22:29:09 +0200

    fixed

--- vektoriai.cpp
+++ vektoriai.cpp
@@ -8,7 +8,7 @@ int main() {
     cout << "2 - Sugeneruoti pažymius" << endl;
     cout << "3 - Sugeneruoti pažymius ir vardus" << endl;
     cout << "4 - Užbaigti programą" << endl;
-    cout << "5 - duomenis nuskaityti iš failo" << endl;
+    cout << "5 - Duomenis nuskaityti iš failo" << endl;
     cin >> choice;
 
     while (cin.fail() || (choice < 1 || choice > 5)) {
